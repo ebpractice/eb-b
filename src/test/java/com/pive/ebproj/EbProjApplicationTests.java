f05d991d169efa08a91f6abc30e4f96edffeb71f
Morris235 <dltkdah777@gmail.com>
2025-05-13 16:00:09 +0900

    first commit

---
+++ src/test/java/com/pive/ebproj/EbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pive.ebproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
