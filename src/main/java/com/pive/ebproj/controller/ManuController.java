70643574b091369aaf3e51613023833b7c9a6f23
Morris235 <dltkdah777@gmail.com>
2025-05-14 09:41:41 +0900

    2nd commit feature/sm/test

---
+++ src/main/java/com/pive/ebproj/controller/ManuController.java
@@ -0,0 +1,12 @@
+package com.pive.ebproj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ManuController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive!";
+    }
+}
